9c9e9ec99342b692b70e5be3931ee8fcc20becb8
Axiom <plwest.axiom@gmail.com>
2017-03-03 23:00:52 -0500

    Implemented Android Multi-Screen Quickstart

---
+++ PhoneWord/PhoneWord/Activities/MainActivity.cs
@@ -0,0 +1,79 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Android.Content;
+using PhoneWord.Interfaces;
+using System.Collections.Generic;
+
+namespace PhoneWord
+{
+    [Activity(Label = "Phone Word", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        static readonly List<string> phoneNumbers = new List<string>();
+        public object Core { get; private set; }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.phoneNumberText);
+            Button translateButton = FindViewById<Button>(Resource.Id.translateButton);
+            Button callButton = FindViewById<Button>(Resource.Id.callButton);
+            Button callHistoryButton = FindViewById<Button>(Resource.Id.CallHistoryButton);
+
+            callButton.Enabled = false;
+
+            string translatedNumber = string.Empty;
+
+            translateButton.Click += (object sender, EventArgs e) => 
+            {
+                translatedNumber = PhoneNumberTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    callButton.Text = "Call";
+                    callButton.Enabled = false;
+                }
+                else
+                {
+                    callButton.Text = string.Format("Call {0}", translatedNumber);
+                    callButton.Enabled = true;
+                }
+            };
+
+            callButton.Click += (object sender, EventArgs e) =>
+            {
+                var callDialog = new AlertDialog.Builder(this);
+                callDialog.SetMessage(string.Format("Call {0}?", translatedNumber));
+                callDialog.SetNeutralButton("Call", delegate
+                                                    {
+                                                        phoneNumbers.Add(translatedNumber);
+
+                                                        callHistoryButton.Enabled = true;
+
+                                                        var callIntent = new Intent(Intent.ActionCall);
+                                                        callIntent.SetData(Android.Net.Uri.Parse(string.Format("tel:{0}?", translatedNumber)));
+                                                        StartActivity(callIntent);
+                                                    });
+                callDialog.SetNegativeButton("Cancel", delegate { });
+
+                callDialog.Show();
+            };
+
+            callHistoryButton.Click += (object sender, EventArgs e) =>
+            {
+                var intent = new Intent(this, typeof(CallHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
+            };
+
+        }
+    }
+}
+
---
+++ PhoneWord/PhoneWord/Implementations/PhoneNumberTranslator.cs
@@ -0,0 +1,81 @@
+﻿using System.Text;
+
+namespace PhoneWord
+{
+    public static class PhoneNumberTranslator
+    {
+
+        /// <summary>
+        /// To the number.
+        /// </summary>
+        /// <param name="raw">The raw.</param>
+        /// <returns></returns>
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+                return string.Empty;
+            else
+                raw = raw.ToUpperInvariant();
+
+            var newNumber = new StringBuilder();
+            foreach (var c in raw)
+            {
+                if (" -0123456789".Contains(c))
+                    newNumber.Append(c);
+                else
+                {
+                    var result = TranslateToNumber(c);
+                    if (result != null)
+                        newNumber.Append(result);
+                }
+            }
+
+
+            return newNumber.ToString();
+        }
+
+
+        /// <summary>
+        /// Translates to number.
+        /// </summary>
+        /// <param name="c">The c.</param>
+        /// <returns></returns>
+        private static object TranslateToNumber(char c)
+        {
+            if ("ABC".Contains(c))
+                return 2;
+            else if ("DEF".Contains(c))
+                return 3;
+            else if ("GHI".Contains(c))
+                return 4;
+            else if ("JKL".Contains(c))
+                return 5;
+            else if ("MNO".Contains(c))
+                return 6;
+            else if ("PQRS".Contains(c))
+                return 7;
+            else if ("TUV".Contains(c))
+                return 8;
+            else if ("WXYZ".Contains(c))
+                return 9;
+
+            return null;
+
+        }
+
+
+        /// <summary>
+        /// Determines whether [contains] [the specified c].
+        /// </summary>
+        /// <param name="keyString">The key string.</param>
+        /// <param name="c">The c.</param>
+        /// <returns>
+        ///   <c>true</c> if [contains] [the specified c]; otherwise, <c>false</c>.
+        /// </returns>
+        static bool Contains(this string keyString, char c)
+        {
+            return keyString.IndexOf(c) >= 0;
+        }
+
+    }
+}
